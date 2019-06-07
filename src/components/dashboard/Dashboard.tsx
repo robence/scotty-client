0a3f50b84b3a8db1e95ad022734dc95ce0b93e54
rozsabence <robence@gmail.com>
2019-06-07 15:49:38 +0200

    remove unused code

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,22 +1,6 @@
 import React from 'react';
 
-// import Spinner from '../ui/Spinner';
-// import Card from '../ui/Card';
-
-// function getCards(): JSX.Element[] {
-//   return Array(5)
-//     .fill(null)
-//     .map(
-//       (_, i): JSX.Element => (
-//         <Card key={i}>
-//           <Spinner loader={`Loader${i + 1}`} />
-//         </Card>
-//       ),
-//     );
-// }
-
 export default function Dashboard(): JSX.Element {
-  // const cardList = getCards();
   const cardList: JSX.Element[] = [];
   return <div className="dashboard">{cardList}</div>;
 }
