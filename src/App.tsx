34cbcc2e5dc457f60a0b115d32e7f6a802b0ea63
rozsabence <robence@gmail.com>
2019-08-16 21:26:53 +0200

    🐛 fix message config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Header from "./layout/Header";
-import Main from "./layout/Main";
-import styled from "styled-components";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './layout/Header';
+import Main from './layout/Main';
+import styled from 'styled-components';
+import { message } from 'antd';
+
+message.config({
+  top: 60,
+  duration: 2,
+});
 
 const App = styled.div`
   background-color: rgb(224, 223, 223);
