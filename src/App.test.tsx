a744109452c5da46296db394352576f23cca0451
rozsabence <robence@gmail.com>
2019-08-17 19:18:24 +0200

    🐛 fix App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './store/rootReducer';
+
+const store = createStore(rootReducer);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
