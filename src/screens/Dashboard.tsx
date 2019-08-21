b20857a71b9d08e304ace32e60961473201d13e6
rozsabence <robence@gmail.com>
2019-08-21 20:08:53 +0200

    ➖ remove precommit hooks

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Carousel } from 'antd';
-
 import AllExpenses from '../components/tables/ExpenseTableContainer';
 import ExpensesByCategory from '../components/tables/CategoryTableContainer';
 import ExpensesByTags from '../components/tables/TagTableContainer';
